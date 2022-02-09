9ca404d2fe0ddc246829703eb73b39de16413eb1
“CrowniEme” <emecrownie@gmail.com>
2022-02-09 12:11:53 +0000

    static fix

--- Downloads/Hello_world/web_project/settings.py
+++ Downloads/Hello_world/web_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static_collected'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
